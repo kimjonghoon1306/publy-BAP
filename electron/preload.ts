0c8e4a155e18460f81f55dbcd37b4df5c8c80e50
kimjonghoon1306 <s965300111@gmail.com>
2026-05-25 21:11:26 +0900

    Update preload.ts

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,5 +1,7 @@
 import { contextBridge, ipcRenderer } from "electron";
 contextBridge.exposeInMainWorld("electron", {
-  getBotStatus: () => ipcRenderer.invoke("get-bot-status"),
-  registerUser: (userId: string) => ipcRenderer.invoke("register-user", userId),
+  getBotStatus:    () => ipcRenderer.invoke("get-bot-status"),
+  getBotSecret:    () => ipcRenderer.invoke("get-bot-secret"),
+  registerUser:    (userId: string) => ipcRenderer.invoke("register-user", userId),
+  unregisterUser:  (userId: string) => ipcRenderer.invoke("unregister-user", userId),
 });
